dcde04632aedbed1007eec927348fbbea6960cbf
phildinuzzo <phil248@gmail.com>
2013-06-14 02:21:31 -0700

    db

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Thelistsf::Application.routes.draw do
   resources :shows
 
-  post '/saved' => 'shows#save_show'
+  post '/save_show' => 'shows#save_show'
 
   root :to => "home#index"
   resources :users, :only => [:index, :show, :edit, :update ]
