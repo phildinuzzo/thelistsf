0e68d590578b34c62dcbd285f293c8c7d7e826b3
phildinuzzo <phil248@gmail.com>
2014-11-16 20:13:16 -0800

    fixed helper require statements

--- app/controllers/test.rb
+++ app/controllers/test.rb
@@ -1,12 +1,12 @@
-def solution(s)
-  if s.size == 1
-  elsif s.size.odd?
-      middle = s.size /2
-      middle.times do |i|
-      if s[i] == s[-i]
-        return middle
-      else
-        return -1
-      end
-  end
-end
+# def solution(s)
+#   if s.size == 1
+#   elsif s.size.odd?
+#       middle = s.size /2
+#       middle.times do |i|
+#       if s[i] == s[-i]
+#         return middle
+#       else
+#         return -1
+#       end
+#   end
+# end
